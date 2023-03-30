1bcc72c1231ffc3ec80c480f72e8b495fe99015a
Erik Rollwage <126534032+Xenrathe@users.noreply.github.com>
2023-03-30 11:13:21 -0400

    Add solution to exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if (numOne < 0 || numTwo < 0)
+        return 'ERROR'
+    else if (typeof numOne != 'number' || typeof numTwo != 'number')
+        return 'ERROR'
+    else{
+        let smallNum = numOne;
+        let largeNum = numTwo;
+        if (numOne > numTwo){
+            smallNum = numTwo;
+            largeNum = numOne;
+        }
 
+        let sum = 0;
+        for (;smallNum <= largeNum; smallNum++){
+            sum += smallNum;
+        }
+
+        return sum
+    }
 };
 
 // Do not edit below this line
