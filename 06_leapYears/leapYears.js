38a308554618545e787d1a697932c9d7ee5df3c0
Erik Rollwage <126534032+Xenrathe@users.noreply.github.com>
2023-03-30 11:17:34 -0400

    Add solution for exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
+const leapYears = function(yearInput) {
+    if (yearInput%4 === 0 && (yearInput%100 !== 0 || yearInput%400 === 0))
+        return true
 
+    return false
 };
 
 // Do not edit below this line
