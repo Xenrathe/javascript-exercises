91139228ff69bf35a46769d4c1b8f4de02f08c2e
Erik Rollwage <126534032+Xenrathe@users.noreply.github.com>
2023-03-30 10:46:12 -0400

    Add solution for exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...args) {
+    let outputArray = inputArray.filter((item) => !args.includes(item));
+    return outputArray
 };
 
 // Do not edit below this line
