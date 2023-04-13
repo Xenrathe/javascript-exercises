9db774616c195ed668e69f8c9160900f8ed66553
Xenrathe <126534032+Xenrathe@users.noreply.github.com>
2023-04-13 18:51:01 -0400

    Add solution for palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (testStr) {
+    testStr = testStr.toLowerCase();
+    testStr = testStr.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    testStr = testStr.replace(/\s/g, '');
 
+    for (let i = 0; i < testStr.length; i++){
+        if (testStr.charAt(i) !== testStr.charAt(testStr.length - i - 1))
+            return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
