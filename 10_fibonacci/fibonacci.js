1e469e79c58d1a38bca4b61feecdb5c09b4b1228
Xenrathe <126534032+Xenrathe@users.noreply.github.com>
2023-04-13 19:02:40 -0400

    Add solution to fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(indexNum) {
+    indexNum = Number(indexNum);
+    if (indexNum < 0)
+        return "OOPS";
 
+    fibSeq = [1, 1];
+
+    for (let i = 2; i < indexNum; i++){
+        fibSeq.push(fibSeq[i-1] + fibSeq[i-2]);
+    }
+    
+    return fibSeq[indexNum-1];  
 };
 
 // Do not edit below this line
