57b91a42ff4dd54a6f9774f290bf3f398a1216a6
Xenrathe <126534032+Xenrathe@users.noreply.github.com>
2023-04-13 19:34:42 -0400

    Add solution to findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleList) {
+    let oldestAge = 0;
+    let oldestPerson = null;
 
+    peopleList.forEach((person) => {
+        let deathYear = 2023;
+    
+        if (person.yearOfDeath !== undefined)
+            deathYear = person.yearOfDeath;
+        
+        if (deathYear - person.yearOfBirth > oldestAge){
+            oldestAge = deathYear - person.yearOfBirth;
+            oldestPerson = person;
+        }
+    });
+
+    return oldestPerson
 };
 
 // Do not edit below this line
