a64940010c88f7fda0cfeb26a370fa4ef5bf98b2
Xenrathe <126534032+Xenrathe@users.noreply.github.com>
2023-04-13 18:25:03 -0400

    Add solution for calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let total = 0;
+	for (let i = 0; i < numbers.length; i++){
+    total += numbers[i];
+  }
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  let total = 1;
+	for (let i = 0; i < numbers.length; i++){
+    total *= numbers[i];
+  }
 
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return Math.pow(base, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  for (;num > 0; num--){
+    total *= num;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
